b6cafe6c2f4d106508d9329932db2668abe46742
Jason Duffy <jasonsduffy@gmail.com>
2021-12-01 00:32:09 -0500

    day1 part1

--- src/day01.rs
+++ src/day01.rs
@@ -1,8 +1,23 @@
 use crate::input;
 
 pub fn run() {
-    let contents = input::get_contents("day01");
-    println!("part1: {}", contents);
+    let contents = input::get_lines("day01");
+    let increases = count_increases(&contents);
+    println!("part1: {:?}", increases);
+}
+
+fn count_increases(report: &[String]) -> u32 {
+    let depths = report
+        .iter()
+        .map(|x| x.parse::<u32>().unwrap())
+        .collect::<Vec<u32>>();
+    let mut result = 0;
+    depths.iter().enumerate().for_each(|(i, &depth)| {
+        if i > 0 && depth > depths[i - 1] {
+            result += 1
+        }
+    });
+    result
 }
 
 #[cfg(test)]
@@ -11,6 +26,18 @@ mod tests {
 
     #[test]
     fn example1() {
-        assert_eq!(1, 2)
+        let report = vec![
+            "199".to_string(),
+            "200".to_string(),
+            "208".to_string(),
+            "210".to_string(),
+            "200".to_string(),
+            "207".to_string(),
+            "240".to_string(),
+            "269".to_string(),
+            "260".to_string(),
+            "263".to_string(),
+        ];
+        assert_eq!(count_increases(&report), 7);
     }
 }
