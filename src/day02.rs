18b9cc1ef2e05184b572e4828c6994231b7a0165
Jason Duffy <jasonsduffy@gmail.com>
2021-12-02 00:28:07 -0500

    day2 part2

--- src/day02.rs
+++ src/day02.rs
@@ -7,12 +7,32 @@ pub fn run() {
     let result = navigate(&contents);
 
     println!("part1: {:?}", result.0 * result.1);
+
+    let result2 = corrected(&contents);
+
+    println!("part2: {:?}", result2.0 * result2.1);
 }
 
 enum Instruction {
     Forward(i32),
     Down(i32),
     Up(i32),
+}
+
+fn corrected(input: &[String]) -> (i32, i32) {
+    let mut result = (0, 0);
+    let mut aim = 0;
+    for line in input {
+        match parse_instruction(line) {
+            Instruction::Down(d) => aim += d,
+            Instruction::Forward(f) => {
+                result.0 += f;
+                result.1 += aim * f;
+            }
+            Instruction::Up(u) => aim -= u,
+        }
+    }
+    result
 }
 
 fn navigate(input: &[String]) -> (i32, i32) {
@@ -42,16 +62,22 @@ fn parse_instruction(line: &str) -> Instruction {
 mod tests {
     use super::*;
 
+    const input: [&str; 6] = [
+        "forward 5",
+        "down 5",
+        "forward 8",
+        "up 3",
+        "down 8",
+        "forward 2",
+    ];
+
     #[test]
     fn example1() {
-        let input = [
-            "forward 5".to_string(),
-            "down 5".to_string(),
-            "forward 8".to_string(),
-            "up 3".to_string(),
-            "down 8".to_string(),
-            "forward 2".to_string(),
-        ];
-        assert_eq!(navigate(&input), (15, 10));
+        assert_eq!(navigate(&input.map(|x| x.to_string())), (15, 10));
+    }
+
+    #[test]
+    fn example2() {
+        assert_eq!(corrected(&input.map(|x| x.to_string())), (15, 60))
     }
 }
