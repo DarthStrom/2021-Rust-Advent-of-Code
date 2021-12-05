dccb50c9fe12061ac0d63cb4873b08ee1539d810
Jason Duffy <jasonsduffy@gmail.com>
2021-12-05 10:49:14 -0500

    day5 part2

--- src/day05.rs
+++ src/day05.rs
@@ -5,9 +5,13 @@ use crate::input;
 pub fn run() {
     let contents = input::get_lines("day05");
 
-    let part1 = overlaps(&contents);
+    let part1 = h_v_overlaps(&contents);
 
     println!("part1: {:?}", part1);
+
+    let part2 = h_v_d_overlaps(&contents);
+
+    println!("part2: {:?}", part2);
 }
 
 fn parse(lines: &[String]) -> Vec<((u32, u32), (u32, u32))> {
@@ -30,7 +34,7 @@ fn parse_coordinate(coordinate: &mut Split<char>) -> u32 {
     coordinate.next().unwrap().parse::<u32>().unwrap()
 }
 
-fn overlaps(lines: &[String]) -> usize {
+fn h_v_overlaps(lines: &[String]) -> usize {
     let coordinates = parse(lines);
     let mut all_coordinates = HashMap::new();
 
@@ -46,6 +50,43 @@ fn overlaps(lines: &[String]) -> usize {
             let range = if x1 < x2 { *x1..=*x2 } else { *x2..=*x1 };
             for x in range {
                 let entry = all_coordinates.entry((x, *y1)).or_insert(0);
+                *entry += 1;
+            }
+        }
+    });
+
+    all_coordinates.into_iter().filter(|e| e.1 > 1).count()
+}
+
+fn h_v_d_overlaps(lines: &[String]) -> usize {
+    let coordinates = parse(lines);
+    let mut all_coordinates = HashMap::new();
+
+    coordinates.iter().for_each(|((x1, y1), (x2, y2))| {
+        if x1 == x2 {
+            for y in if y1 < y2 { *y1..=*y2 } else { *y2..=*y1 } {
+                let entry = all_coordinates.entry((*x1, y)).or_insert(0);
+                *entry += 1;
+            }
+        } else if y1 == y2 {
+            for x in if x1 < x2 { *x1..=*x2 } else { *x2..=*x1 } {
+                let entry = all_coordinates.entry((x, *y1)).or_insert(0);
+                *entry += 1;
+            }
+        } else {
+            let xrange: Box<dyn Iterator<Item = _>> = if x1 < x2 {
+                Box::new(*x1..=*x2)
+            } else {
+                Box::new((*x2..=*x1).rev())
+            };
+            let yrange: Box<dyn Iterator<Item = _>> = if y1 < y2 {
+                Box::new(*y1..=*y2)
+            } else {
+                Box::new((*y2..=*y1).rev())
+            };
+            let range = xrange.zip(yrange);
+            for (x, y) in range {
+                let entry = all_coordinates.entry((x, y)).or_insert(0);
                 *entry += 1;
             }
         }
@@ -109,6 +150,13 @@ mod tests {
     fn example1() {
         let lines = INPUT.map(|s| s.to_string());
 
-        assert_eq!(overlaps(&lines), 5);
+        assert_eq!(h_v_overlaps(&lines), 5);
+    }
+
+    #[test]
+    fn example2() {
+        let lines = INPUT.map(|s| s.to_string());
+
+        assert_eq!(h_v_d_overlaps(&lines), 12);
     }
 }
