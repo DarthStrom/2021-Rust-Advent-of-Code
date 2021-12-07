831ec2accf8c7fe01956b4d1bbbe0dc6f3e54c95
Jason Duffy <jasonsduffy@gmail.com>
2021-12-07 07:52:14 -0500

    refactor

--- src/day07.rs
+++ src/day07.rs
@@ -35,22 +35,21 @@ fn cost_corrected(positions: &[i32], destination: i32) -> i32 {
 }
 
 fn least_fuel(positions: &[i32]) -> (i32, i32) {
-    let mut result = (-1, i32::MAX);
-    let max = positions.iter().max().unwrap();
-    for x in 0..=*max {
-        let cost = cost(positions, x);
-        if cost < result.1 {
-            result = (x, cost);
-        }
-    }
-    result
+    least_fuel_by(positions, cost)
 }
 
 fn least_fuel_corrected(positions: &[i32]) -> (i32, i32) {
+    least_fuel_by(positions, cost_corrected)
+}
+
+fn least_fuel_by<F>(positions: &[i32], cost_func: F) -> (i32, i32)
+where
+    F: Fn(&[i32], i32) -> i32,
+{
     let mut result = (-1, i32::MAX);
     let max = positions.iter().max().unwrap();
     for x in 0..=*max {
-        let cost = cost_corrected(positions, x);
+        let cost = cost_func(positions, x);
         if cost < result.1 {
             result = (x, cost);
         }
